6e8edec9cf77f1d1faf31189ae88b56a82f3ffc9
vincent <vincent@vboot.vincent>
2017-05-10 11:01:48 +0200

    loginregster

---
+++ app.js
@@ -0,0 +1,58 @@
+// declaring dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Conncet to DB
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+  console.log('connected to database '+config.database);
+});
+
+
+// Error connection
+mongoose.connection.on('error', (err) => {
+  console.log('database error '+err);
+});
+
+
+ //initialize app variable
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+// cors parser middleware
+app.use(cors());
+
+//Set static folder file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// body parser middleware
+app.use(bodyParser.json());
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+// index route
+app.get('/', (req, res) =>  {
+  res.send('invalid endpoint')
+});
+
+// start server
+//arrow function  (lighter synthax)
+app.listen(port, () => {
+  console.log('server started on port '+port);
+});
